ef3f991a5bdfbf7090162eeaed271bd49772a8c3
Andy Smith <andysmith.d@gmail.com>
2016-06-08 14:29:17 +0100

    PEP 8

--- portal/forms.py
+++ portal/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class EmailLinkForm(forms.Form):
     email = forms.EmailField(label='Email', max_length=254,
-                             help_text="Please enter the email address associated with \
-                             your projects")
+                             help_text="Please enter the email address "
+                             "associated with your projects")
