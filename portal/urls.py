a79d78b6d15bf59f2574198913882aa41d98fe12
Andy Smith <andysmith.d@gmail.com>
2016-06-09 10:33:10 +0100

    Add basic project view + url + template for viewing project status

--- portal/urls.py
+++ portal/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^email-link/$', views.email_link, name='email_link'),
+    url(r'^projects/(?P<uuid>[-\w]{36})/$', views.project, name='project'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -11,11 +11,64 @@ from django.utils.http import urlencode
 from .forms import EmailLinkForm
 
 
-def email_link(request):
-    success_message = "Thanks! Your project links should arrive in " \
-                      "your inbox shortly."
+def project(request, uuid):
     failure_message = "We're experiencing some problems right now, " \
                       "please try again later."
+
+    # make project api request
+    project_url = (settings.RESTFM_BASE_URL +
+                   'layout/project_api.json?' +
+                   urlencode({
+                       'RFMkey': settings.RESTFM_KEY,
+                       'RFMsF1': 'uuid',
+                       'RFMsV1': '==' + uuid,
+                   })
+                   )
+    try:
+        api_response = requests.get(project_url, timeout=5)
+        project = api_response.json()['data'][0]
+        projectline_url = (settings.RESTFM_BASE_URL +
+                           'layout/projectline_api.json?' +
+                           urlencode({
+                               'RFMkey': settings.RESTFM_KEY,
+                               'RFMsF1': 'project_id',
+                               'RFMsV1': project['project_id'],
+                               'RFMmax': 0,
+                           })
+                           )
+        api_response = requests.get(projectline_url, timeout=5)
+        projectlines = api_response.json()['data']
+    except Exception:
+        messages.error(request, failure_message)
+        return render(request, 'portal/project.html')
+
+    # Django templates can't handle '::' in keys
+    for pl in projectlines:
+        for key in pl:
+            if ':' in key:
+                pl[key.replace(':', '_')] = pl.pop(key)
+    projectlines.sort(key=lambda k:
+                      (
+                          k['Container__reference'],
+                          int(k['Aliquot__unstored_container_position']),
+                          k['Sample__reference'],
+                      )
+                      )
+    print(projectlines)
+    project['projectlines'] = projectlines
+
+    return render(request, 'portal/project.html',
+                  {
+                      'project': project,
+                  }
+                  )
+
+
+def email_link(request):
+    success_message = "Thanks! Your project links should arrive in " \
+        "your inbox shortly."
+    failure_message = "We're experiencing some problems right now, " \
+        "please try again later."
 
     if request.method == 'POST':
         # honeypot
