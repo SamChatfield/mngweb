8955066331901990182e115d0edb8920c523263c
Andy Smith <andysmith.d@gmail.com>
2016-06-10 17:52:38 +0100

    Make it possible to dismiss popovers by clicking anywhere outside

--- static/js/mngweb.js
+++ static/js/mngweb.js
@@ -125,4 +125,15 @@ $(function() {
     ajax_post(this);
   });
 
+  // Close popovers when clicking outside
+  $('body').on('click', function (e) {
+    $('[data-toggle="popover"]').each(function () {
+        //the 'is' for buttons that trigger popups
+        //the 'has' for icons within a button that triggers a popup
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+});
+
 });
