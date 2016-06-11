fdcd62138a7345789ca8d5d3e92deb0409c84d44
Andy Smith <andysmith.d@gmail.com>
2016-06-11 17:15:56 +0100

    Move common templates from PROJECT_ROOT to BASE_DIR

--- source/mngweb/settings/base.py
+++ source/mngweb/settings/base.py
@@ -76,7 +76,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(PROJECT_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
