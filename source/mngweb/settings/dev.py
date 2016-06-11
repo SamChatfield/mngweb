643aa2bc6e179922f414daaa77ee673b489a7b86
Andy Smith <andysmith.d@gmail.com>
2016-06-11 16:30:56 +0100

    Move EMAIL_BACKEND from local.py to prodction.py and dev.py

--- source/mngweb/settings/dev.py
+++ source/mngweb/settings/dev.py
@@ -7,8 +7,9 @@ DEBUG = True
 for template_engine in TEMPLATES:
     template_engine['OPTIONS']['debug'] = True
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 LIMS_STATS_CACHE_TIMEOUT = 500
 
--- source/mngweb/settings/production.py
+++ source/mngweb/settings/production.py
@@ -5,6 +5,8 @@ DEBUG = False
 
 LIMS_STATS_CACHE_TIMEOUT = 86400  # 24 hours
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 try:
     from .local import *
 except ImportError:
