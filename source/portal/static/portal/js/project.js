e470e6a5b4452e110505d8ff508f27430cdc75c7
Andy Smith <andysmith.d@gmail.com>
2016-06-20 12:23:05 +0100

    Make correct meta fields show for forms that have initial data

--- source/portal/static/portal/js/project.js
+++ source/portal/static/portal/js/project.js
@@ -14,33 +14,37 @@ $(function() {
     $('.meta-data-environmental input', context).val('');
   }
 
-  hideMetaFields(this);
+  function showMetaFields(context) {
+    var studyType = $('#id_study_type', context).val();
+    hideMetaFields(context);
+    switch (studyType) {
+      case 'Lab':
+        $('.meta-data-lab', context).show();
+        var fdPlaceholder = $('#lab-fd-placeholder').text();
+        $('#id_further_details', context).attr("placeholder", fdPlaceholder);
+        $('.meta-data-further-details', context).attr("placeholder", fdPlaceholder).show();
+        break;
+      case 'Host':
+        $('.meta-data-host', context).show();
+        var fdPlaceholder = $('#host-fd-placeholder').text();
+        $('#id_further_details', context).attr("placeholder", fdPlaceholder);
+        $('.meta-data-further-details', context).show();
+        break;
+      case 'Environmental':
+        $('.meta-data-environmental', context).show();
+        var fdPlaceholder = $('#env-fd-placeholder').text();
+        $('#id_further_details', context).attr("placeholder", fdPlaceholder);
+        $('.meta-data-further-details', context).show();
+        break;
+    }
+  }
+
+  showMetaFields(this);
 
   $('select[name="study_type"]').change(function() {
       var context = $(this).parent().parent();
-      var studyType = $(this).val();
       clearMetaFields(context);
-      hideMetaFields(context);
-      switch (studyType) {
-        case 'lab':
-          $('.meta-data-lab', context).show();
-          var fdPlaceholder = $('#lab-fd-placeholder').text();
-          $('#id_further_details', context).attr("placeholder", fdPlaceholder);
-          $('.meta-data-further-details', context).attr("placeholder", fdPlaceholder).show();
-          break;
-        case 'host':
-          $('.meta-data-host', context).show();
-          var fdPlaceholder = $('#host-fd-placeholder').text();
-          $('#id_further_details', context).attr("placeholder", fdPlaceholder);
-          $('.meta-data-further-details', context).show();
-          break;
-        case 'environmental':
-          $('.meta-data-environmental', context).show();
-          var fdPlaceholder = $('#env-fd-placeholder').text();
-          $('#id_further_details', context).attr("placeholder", fdPlaceholder);
-          $('.meta-data-further-details', context).show();
-          break;
-      }
+      showMetaFields(context);
   });
 
   // AJAX post on submit
