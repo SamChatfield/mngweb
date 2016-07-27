fb47d0589eab73536bea78638178f96c00b98b97
Andy Smith <andysmith.d@gmail.com>
2016-07-27 13:38:13 +0100

    Change localisation TIME_ZONE and LANGUAGE_CODE

--- source/mngweb/settings/base.py
+++ source/mngweb/settings/base.py
@@ -102,9 +102,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
