f34684e4cb00fab46d1b436fb34d76f0b5fc0849
Andy Smith <andysmith.d@gmail.com>
2016-09-05 12:15:45 +0100

    Taxon model default order by name

--- source/taxon/models.py
+++ source/taxon/models.py
@@ -9,5 +9,8 @@ class Taxon(models.Model):
     data_set = models.CharField(max_length=20)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
