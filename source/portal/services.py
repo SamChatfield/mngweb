9eebecb237d0a41e5505c984366ae2655e645e7b
Andy Smith <andysmith.d@gmail.com>
2016-12-19 11:35:10 +0000

    Remove lims_project_auto_queue function

--- source/portal/services.py
+++ source/portal/services.py
@@ -347,12 +347,6 @@ def limsfm_bulk_update_projectlines(project_uuid, projectlines):
     return update_response
 
 
-def limsfm_project_auto_queue(project_uuid):
-    """Call a script to auto queue project lines after data submission, where appropriate"""
-    uri = 'script/project_auto_queue_after_data_submission/REST'
-    return limsfm_request(uri, 'get', params={'RFMscriptParam': project_uuid})
-
-
 def limsfm_get_taxonomy(data_set=None, q=None):
     """Return taxonomy as a list of dictionaries"""
     uri = ('layout/taxon_api')
