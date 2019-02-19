b1c133729dfbaf5f55d8ee7fa88ba59aaa7dde10
Sam Chatfield <samchatfield97@gmail.com>
2019-02-19 15:53:54 +0000

    Remove config options from celery config module, because they are moved to local settings. Remove placeholder tasks from portal tasks module.

--- source/portal/tasks.py
+++ source/portal/tasks.py
@@ -1,17 +1,6 @@
 import subprocess
 from celery import shared_task
 
-@shared_task
-def run_subprocess():
-    # We can return a Python object which will be serialised and sent
-    obj = {}
-    # Note we must decode the return value of subprocess.check_output as UTF-8 string
-    obj['outstr'] = subprocess.check_output(['conda', '-h']).decode('utf-8')
-    return obj
-
-@shared_task
-def touch_file():
-    return subprocess.call(['touch', '/home/ubuntu/CELERYTEST.txt'])
 
 @shared_task
 def mngvariants(uuid, samples, reference):
